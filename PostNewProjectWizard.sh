43fed1c8a0190c37a6ab262b80820b084629b429
Gabriel Bsutillo del Cuvillo <gabibust@gmail.com>
2020-01-02 00:33:38 +0100

    Replaced scripts files with Symbolic Links and added UE4-Toolkit-Public as submodule

--- PostNewProjectWizard.sh
+++ PostNewProjectWizard.sh
@@ -1,11 +1,1 @@
-#!/bin/sh
-#NAME=`git config user.name`
-#LICENSE="Apache License 2.0"
-
-$SOURCE="./Source/*/"
-mkdir -p 	$SOURCE/Character \
-		$SOURCE/AI \
-		$SOURCE/Core \
-		$SOURCE/ActorComponents
-
-# echo "Copyright © 2019 - $NAME"
+./Tools/PostNewProjectWizard.sh
