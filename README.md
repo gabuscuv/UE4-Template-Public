3de9ddafd62174e9c14a6b8f82e7a6fb2c211673
Gabriel Bustillo del Cuvillo <gabibust@gmail.com>
2020-01-01 18:58:41 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# UE4-Template
+Template for UE4 Projects
+Please check first the global variables of each script before run any script.
+
---
+++ cooking.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Init Variable
+oldPWD=$PWD
+uproject=`ls "$oldPWD"/*.uproject`
+
+#Writable Variables
+UE4DIR="/opt/UE4"
+OUTPUT="$oldPWD/Build"
+CPFiles="README LICENSE CHANGELOG.txt"
+VERSION="0.0.1"
+PROFILE="Development"
+
+cd $UE4DIR
+
+Engine/Build/BatchFiles/RunUAT.sh \
+BuildCookRun \
+	-project="$uproject" -noP4 -platform=Linux \
+ 	-clientconfig=$PROFILE \
+	-cook -allmaps -build -stage -pak -archive \
+	-archivedirectory="$oldPWD/Build"
+cd $oldPWD
+for file in $CPFiles; do
+    if [[ -f "$file" ]]; then
+        cp $file "$OUTPUT"/*/
+    fi
+done
+
+7z a -t7z $(basename "$uproject" .uproject)-Linux-$VERSION.7z -m0=lzma2 -mx=9 -aoa -mmt=on "$OUTPUT"/*/*
